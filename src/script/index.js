2938c71a06998a63bb393d54bbfdd0b5788789fd
João Coelho <joaohg14@gmail.com>
2024-07-01 19:32:25 -0300

    Tarefa 02 concluída

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,15 @@
 // Script global
+
+// Ativação do botão de genero
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.gender__list button');
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', function() {
+        // Remover a classe 'active' de todos os botões
+        buttons.forEach(btn => btn.classList.remove('active'));
+        // Adicionar a classe 'active' apenas ao botão clicado
+        this.classList.add('active');
+      });
+    });
+  });
